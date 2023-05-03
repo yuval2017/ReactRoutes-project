dffde82d198cbca7e845b97d0740e93bf6842317
yuval2017 <hitter@post.bgu.ac.il>
2023-05-03 09:26:45 +0300

    change header color

--- van-life/src/App.js
+++ van-life/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import './server'
 function App() {
   return (
     <div className="App">
